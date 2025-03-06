25076daabe08cf9fa6c37b1a2e54e80c96c400e1
Aria 3 <ariarock306@gmail.com>
2025-03-07 05:00:53 +0800

    Update spawn.js
    fixes: Made Sure it tps immediately upon spawn

--- src/minecraft/events/general/spawn.js
+++ src/minecraft/events/general/spawn.js
@@ -10,6 +10,8 @@ module.exports = () => {
 
 	logInfo(`✅ Bot has spawned! - Ensure to check if this is following a respawn/death event. Bot logged in as ${minecraftBot.player.displayName}`);
 
+	minecraftBot.chat(`/tp ${process.env.TP_TO_USERNAME}`);
+	
 	// Start a new teleportation interval
 	teleportInterval = setInterval(() => {
 		minecraftBot.chat(`/tp ${process.env.TP_TO_USERNAME}`);
