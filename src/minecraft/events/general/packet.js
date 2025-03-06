29bb75b6cf8ce853c6a850dc66225d8b436145c0
Aria 3 <ariarock306@gmail.com>
2025-03-07 06:06:02 +0800

    Delete src/minecraft/events/general/packet.js

--- src/minecraft/events/general/packet.js
+++
@@ -1,7 +0,0 @@
-// YO META MENTIONED
-module.exports = (packet, meta) => {
-	if (!["map", "keep_alive"].includes(meta.name)) {
-		// Ignore frequent packets
-		console.log(`📦 Packet received: ${meta.name}`, packet);
-	}
-}
